cb0a3659402796e20af370d82ec80259f2adefc2
Daniel J. Dean <dade@microsoft.com>
2022-08-02 13:44:12 -0400

    Update index.js

--- read_yaml/index.js
+++ read_yaml/index.js
@@ -10,7 +10,7 @@ try {
       return;
     }
     console.log(data);
-    const configYaml = yaml.safeLoad(data, 'FAILSAFE_SCHEMA');
+    const configYaml = yaml.load(data, 'FAILSAFE_SCHEMA');
     const namespace = String(configYaml["variables"]["namespace"])
     const postfix = String(configYaml["variables"]["postfix"])
     const environment = String(configYaml["variables"]["environment"])
